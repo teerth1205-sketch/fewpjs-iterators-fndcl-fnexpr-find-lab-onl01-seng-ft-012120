56df35cb85cd70ac814f1e79af2915dd7011ab19
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-03 22:10:14 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin (Array) {
+    if (!Array.find(function(s) {return s.result == "W"})){
+    return undefined
+    } else{ 
+   let A = Array.find(function(s) {return s.result == "W"}).year
+    return A
+    }
+}
